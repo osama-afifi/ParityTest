a8deba54d990446a6af670fa1774b05cc0eeecc1
Osama Magdy Afifi <osama.egt@gmail.com>
2017-03-03 23:09:39 +0400

    add main for imports

--- api_parity_test.py
+++ api_parity_test.py
@@ -1,11 +1,17 @@
-from requests import async
-
+import async
+import requests
+import csv
 
 #urls = ['http://github.com/osama-afifi']
 
 # A simple task to do to each response object
 def do_something(response):
-    print response.url, response.text
+	with open('output1.csv', 'wb') as csvout:
+    	print response.url, response.text
+		csvout = csv.writer(csvout)
+		#csvout.writerows([row[0,22] for _ in xrange(count)])
+
+
 
 # A list to hold our things to do via async
 
@@ -26,9 +32,47 @@ def make_get_requests(urls):
 	async.map(async_list)
 
 
-def prepare_urls():
-	urls = []
-	make_get_requests(urls)
-
-def start():
+def populateHeader():
 	pass
+
+def populateUrls():
+	urls1
+	limit = 10
+	with open('sample.tsv','rb') as tsvin:
+		tsvin = csv.reader(tsvin, delimiter='\t')
+		
+		line_count = 0
+		for row in tsvin:
+
+			line_count += 1
+
+			if line_count == 1:
+				continue
+
+			if line_count > limit:
+				break
+
+			bookingId = row[0]
+			bookingUUID = row[22]
+			client_id = row[3]
+			print client_id, bookingId, bookingUUID
+			#print row
+
+
+
+
+
+
+def prepare_urls():
+	urls1 = []
+
+	populateUrls()
+
+
+	#make_get_requests(urls)
+
+def main():
+    prepare_urls()
+
+if __name__ == "__main__":
+    main()
